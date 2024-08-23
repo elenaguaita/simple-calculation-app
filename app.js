d6a434e68256d68d4e9feaba78dfc4d4a2843aca
elenuguaita <eleninaguaita@icloud.com>
2024-08-23 15:31:13 +0200

    Build /calculate endpoint (basic structure)

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.use(express.json());  // middleware necessary to parse request body
+app.use(express.static("public"));
+
+
+// /calculate endpoint (accepting a POST request with 2 numbers in the payload)
+app.post("/calculate", (req, res) => {
+    const { first_num, second_num } = req.body;
+    
+    if (typeof first_num === 'number' && typeof second_num === 'number') {  // input validation 
+        
+        // elaboration of the result
+        result = 0  // to be calculated
+
+        // send result in JSON format
+        res.json({ result })
+
+    } else {
+
+        // send error message (response status 400: Bad Request)
+        res.status(400).json({ error: "Invalid input. Please provide two numbers." });  // to be displayed
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
