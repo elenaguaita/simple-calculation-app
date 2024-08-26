d93c25c159bb4bd59ca16dd63f4e4f15c098bba2
elenuguaita <eleninaguaita@icloud.com>
2024-08-26 21:21:00 +0200

    Add EventListener to calculate-btn: send JSON formatted user input to /calculate endpoint via POST request and fetch the response

---
+++ public/script.js
@@ -0,0 +1,14 @@
+document.getElementById('calculate-btn').addEventListener('click', async () => {
+    const first_num = Number(document.getElementById('first-num').value);
+    const second_num = Number(document.getElementById('second-num').value);
+
+    const input_data = JSON.stringify({ first_num: first_num, second_num: second_num });
+
+    const response = await fetch('/calculate', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: input_data
+    })
+})
