cc8d2fe76b4c5f260b376b9eff4e5802f1c217de
elenuguaita <eleninaguaita@icloud.com>
2024-08-26 21:41:45 +0200

    Set / home endpoint to send index.html, make few changes to HTML/CSS (placeholder values and opacity)

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const port = 8080;
 app.use(express.json());  // middleware necessary to parse request body
 app.use(express.static("public"));
 
+app.get('/', (req, res) => {
+    res.sendFile("index.html")
+});
 
 // /calculate endpoint (accepting a POST request with 2 numbers in the payload)
 app.post("/calculate", (req, res) => {
@@ -13,11 +16,10 @@ app.post("/calculate", (req, res) => {
     if (typeof first_num === 'number' && typeof second_num === 'number') {  // input validation 
         
         // elaboration of the result
-        result = 0  // to be calculated
+        const result = first_num + second_num // first bigger prime number to be added
 
         // send result in JSON format
         res.json({ result })
-
     } else {
 
         // send error message (response status 400: Bad Request)
