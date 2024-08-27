04f256893eb51918f157a8d8acf1d380bd873af3
elenuguaita <eleninaguaita@icloud.com>
2024-08-27 10:51:28 +0200

    Implement isPrime and findNextPrime functions to calculate the last number to add to the sum

--- app.js
+++ app.js
@@ -5,6 +5,44 @@ const port = 8080;
 app.use(express.json());  // middleware necessary to parse request body
 app.use(express.static("public"));
 
+function isPrime(number) {
+    // exclude negative and 0, 1
+    if (number < 2) return false;
+    
+    // check if any divider exists
+    for (let divider = 2; divider < number; divider++) {
+        if (number % divider === 0) {
+            // in that case, number is NOT a prime number
+            return false
+        }
+    }
+
+    // otherwise, number is a prime number
+    return true
+}
+
+function findNextPrime(num1, num2) {
+    let bigger = 0
+    let next_prime = 0
+
+    // find which number is bigger
+    if (num1 > num2) {
+        bigger = num1
+    } else {
+        bigger = num2
+    }
+    
+    next_prime = bigger + 1
+
+    // find the next prime number
+    while(!isPrime(next_prime)) {
+        next_prime++
+    }
+
+    return next_prime
+}
+
+// / home endpoint
 app.get('/', (req, res) => {
     res.sendFile("index.html")
 });
@@ -16,7 +54,7 @@ app.post("/calculate", (req, res) => {
     if (typeof first_num === 'number' && typeof second_num === 'number') {  // input validation 
         
         // elaboration of the result
-        const result = first_num + second_num // first bigger prime number to be added
+        const result = first_num + second_num + findNextPrime(first_num, second_num)  // next prime number added
 
         // send result in JSON format
         res.json({ result })
