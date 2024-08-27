596bed9a8950055484f22e29b3cad12f20580af2
elenuguaita <eleninaguaita@icloud.com>
2024-08-27 11:07:10 +0200

    Add operators to the operation HTML section (to display them)

--- public/script.js
+++ public/script.js
@@ -15,4 +15,8 @@ document.getElementById('calculate-btn').addEventListener('click', async () => {
     const output = await response.json();
     document.getElementById('result').textContent = output.result;
     document.getElementById('result-display').style.opacity = 1;
+
+    let prime_num = output.result - first_num - second_num
+    let operation_str = `${first_num} + ${second_num} + ${prime_num}`
+    document.getElementById('operation').textContent = operation_str;
 })
